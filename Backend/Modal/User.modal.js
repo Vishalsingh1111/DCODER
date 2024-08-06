852d4b4ad963dcb8414cfc5cb1373eb7270427e8
vishal singh <vishalsingh@vishals-MacBook-Air-3.local>
2024-08-06 10:54:28 +0530

    Done

--- Backend/Modal/User.modal.js
+++ Backend/Modal/User.modal.js
@@ -1,22 +1,3 @@
-// import mongoose from 'mongoose';
-
-// const userSchema = new mongoose.Schema({
-//     firstName: { type: String, required: false },
-//     lastName: { type: String, required: false },
-//     email: { type: String, required: true, unique: true },
-//     phone: { type: String, required: false },
-//     password: { type: String, required: false },
-//     googleId: { type: String },
-//     photo: { type: String }
-
-// });
-
-// const User = mongoose.model('User', userSchema);
-
-// export default User;
-
-
-
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
@@ -25,7 +6,7 @@ const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     phone: { type: String, required: false, default: "null" },
     password: { type: String, required: false, default: "null" },
-    googleId: { type: String, required: false, default: "null" },
+    googleId: { type: String, required: false, unique: false, default: "null" },
     photo: { type: String, required: false, default: "null" }
 });
 
