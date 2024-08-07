6781c78053b53e4b8c7638eaf51a5ea11c85b76e
vishal singh <vishalsingh@vishals-MacBook-Air-3.local>
2024-08-07 22:26:18 +0530

    DONE

--- Frontend/src/components/Logout.jsx
+++ Frontend/src/components/Logout.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { useAuth } from '../Context/AuthProvider'
+import { useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
 
 
 function Logout() {
 
     const [authUser, setAuthUser] = useAuth();
+    const navigate = useNavigate();
     const handleLogout = () => {
         try {
             setAuthUser({
@@ -16,6 +18,7 @@ function Logout() {
 
             toast.success("Logout Successfully");
             setTimeout(() => {
+                navigate('/');
                 window.location.reload();
             }, 1000)
 
@@ -27,7 +30,7 @@ function Logout() {
 
     return (
         <div>
-            <button className="bg-red-500 text-white px-3.5 py-2 rounded-md hover:bg-red-800 duration-300 cursor-pointer dark:bg-red-800"
+            <button className="bg-red-500 text-white px-3 py-1.5 rounded-md hover:bg-red-800 duration-300 cursor-pointer dark:bg-red-800"
                 onClick={handleLogout}>
                 Logout
             </button>
