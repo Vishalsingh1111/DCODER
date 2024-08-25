50e168ad23777cfa4fdc8f7e366a3c6f22a78be3
vishal singh <vishalsingh@vishals-MacBook-Air-3.local>
2024-08-25 18:20:37 +0530

    h

--- Frontend/src/components/TechBlogs/Blogscard.jsx
+++ Frontend/src/components/TechBlogs/Blogscard.jsx
@@ -8,16 +8,24 @@ const Blogcard = ({ item, onClick }) => {
         >
             <div className="content flex-1 flex flex-col justify-between">
                 <div>
-                    <img src={item.image} className='w-full pb-2' alt="Blog" />
-                    <h3 className="text-lg text-blue-900 font-semibold mb-2 dark:text-white">{item.header}</h3>
-                    <p className="text-gray-600 dark:text-gray-300">{item.category}</p>
+                    <img
+                        src={item.image}
+                        className="w-full h-[150px] object-cover rounded-lg"
+                        alt="Blog"
+                    />
+                    <h3 className="text-lg text-blue-900 my-2 dark:text-white">
+                        {item.header}
+                    </h3>
+                    <p className="text-red-600 dark:text-gray-500">
+                        {item.category}
+                    </p>
                 </div>
                 <div className="flex justify-start mt-4">
                     <a
-                        className="text-purple-500 inline-block border p-2 rounded-lg border-purple-400 group-hover:bg-purple-500 group-hover:text-white group-hover:border-purple-500 transition-all duration-300"
+                        className="text-gray-700 inline-block border p-2 rounded-lg border-gray-500 group-hover:bg-gray-700 group-hover:text-white group-hover:border-gray-700 transition-all duration-300"
                         href="#"
                     >
-                        {"Click to open"} &rarr;
+                        Click to open jsah
                     </a>
                 </div>
             </div>
