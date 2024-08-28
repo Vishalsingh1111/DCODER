810d867dd0f2041bd6a9be01c9c1758387648df8
vishal singh <vishalsingh@vishals-MacBook-Air-3.local>
2024-08-28 19:23:47 +0530

    ADD

--- Frontend/src/components/Projects/Projectcard.jsx
+++ Frontend/src/components/Projects/Projectcard.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Projectcard = ({ item, onClick }) => {
     return (
-        <div onClick={onClick} className="p-8 flex flex-col md:flex-row w-full md:w-[350px] cursor-pointer ">
-            <div className="rounded-xl p-5 md:p-8 hover:shadow-lg dark:bg-slate-800 dark:text-white dark:border-none bg-white border-gray-700 border ">
+        <div onClick={onClick} className="p-4 flex flex-col md:flex-row w-full md:w-[350px] cursor-pointer ">
+            <div className="rounded-xl  md:p-5 hover:shadow-lg dark:bg-slate-800 dark:text-white dark:border-none bg-white border-gray-300 border ">
                 <div className="mb-3 flex items-center justify-center">
-                    {item.image1 && <img src={item.image1} alt="Project thumbnail" className=' dark:border-none rounded-xl' />}
+                    {item.image1 && <img src={item.image1} alt="Project thumbnail" className=' dark:border-none' />}
                 </div>
                 <div className='bg-gray'>
                     <h4 className="mb-2 text-xl text-blue-900 font-semibold text-dark dark:text-white">
@@ -17,10 +17,12 @@ const Projectcard = ({ item, onClick }) => {
                     <p className="mb-2 text-sm dark:text-dark-6">
                         {item.category}
                     </p>
-                    <a className="text-purple-500 inline-block">{"Click for details"} &rarr;</a>
+                    <a className="text-red-500 inline-block">{"Click for details"} &rarr;</a>
                 </div>
             </div>
         </div>
     );
 };
 export default Projectcard;
+
+
