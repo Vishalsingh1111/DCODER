56bd705373f81aa0598e9c92335e8697f152a0f9
vishal singh <vishalsingh@vishals-MacBook-Air-3.local>
2024-10-16 12:35:22 +0530

    add

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -24,12 +24,12 @@ import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import AuthProvider from './Context/AuthProvider.jsx';
-import AuthProvider2 from './Context/AuthProvider2.jsx'; // Import your admin provider
+import AuthProvider2 from './Context/AuthProvider2.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <AuthProvider>
-      <AuthProvider2> {/* Wrap with both providers */}
+      <AuthProvider2>
         <div className='dark:bg-slate-900 dark:text-white'>
           <App />
         </div>
